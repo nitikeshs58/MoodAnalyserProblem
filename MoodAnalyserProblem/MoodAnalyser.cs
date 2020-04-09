421eef0f82cf3bfe1d26bc88495badbad81e522d
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-09 15:30:49 +0530

    Refactor: refactored code and taken mood message in comstructor.

--- MoodAnalyserProblem/MoodAnalyser.cs
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -8,12 +8,23 @@ namespace MoodAnalyserProblem
     {
         string message;
 
+        // Default Constructor
+        public MoodAnalyser()
+        {
+        }
+
+        // Parameterised Constructor
+        public MoodAnalyser(String message)
+        {
+            this.message = message;            
+        }
+
         /// <analyseMood>
         /// Method to return the mood analysis
         /// </analyseMood>
         /// <param name="message"><. mood parameter >
         /// <returns></. Sad or Happy , type: string>
-        public string analyseMood(string message)
+        public string analyseMood()
         {
             if (message.Contains("sad"))
             {
--- MoodAnalyserTest/MoodTest.cs
+++ MoodAnalyserTest/MoodTest.cs
@@ -5,23 +5,41 @@ namespace Tests
 {
     public class Tests
     {
-        MoodAnalyser mood = new MoodAnalyser();
-        [SetUp]
+        MoodAnalyser mood = null;
+
+
+       [SetUp]
         public void Setup()
         {
         }
 
+        /// <checkForSad>
+        /// Calling parameteried constructor
+        /// Calling No parameter analyseMood method
+        /// checking return mood is "sad" are not
+        /// if return mood "sad" then Test passed.
+        /// </checkForSad>
         [Test]
         public void checkForSad()
         {
-            string returnMood = mood.analyseMood("i am in sad mood");            
+            string message = "i am in sad mood";
+            mood =new MoodAnalyser(message);
+            string returnMood = mood.analyseMood();            
             Assert.AreEqual("sad", returnMood);
         }
 
+        /// <checkForHappy>
+        /// Calling parameteried constructor
+        /// Calling No parameter analyseMood method
+        /// checking return mood is "happy" are not
+        /// if return mood "happy" then Test passed.
+        /// </checkForHappy>
         [Test]
         public void checkForHappy()
         {
-            string returnMood = mood.analyseMood("i am in happy mood");
+            string message = "i am in happy mood";
+            mood = new MoodAnalyser(message);
+            string returnMood = mood.analyseMood();
             Assert.AreEqual("happy", returnMood);
         }
     }
